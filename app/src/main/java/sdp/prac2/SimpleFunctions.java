d07ce31a002cbf810e9f82c6006b41be11d9a30e
William <williwiser@gmail.com>
2024-05-18 19:55:31 +0200

    Task 3 done

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,25 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+    }
+
+    public boolean Task3(String s) {
+        char[] characters = s.toCharArray();
+        int stack = 0;
+        for (int i = 0; i < characters.length; i++) {
+            if (stack == 0 && characters[i] == ')') {
+                return false;
+            }
+            if (characters[i] == '(') {
+                stack++;
+            } else if (characters[i] == ')') {
+                stack--;
+            }
+        }
+        if (stack == 0) {
+            return true;
+        } else
+            return false;
+    }
 }
