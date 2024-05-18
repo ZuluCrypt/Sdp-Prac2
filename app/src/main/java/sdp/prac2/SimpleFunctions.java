93178a2a95b18d4a2b72e33af94b516923cb8951
Zulu crypt <g20z6722@campus.ru.ac.za>
2024-05-18 22:46:30 +0200

    fixed Task4 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,16 +4,16 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    //reverse list b and store in reversedList
+    public static List<Integer> reverseList(List<Integer> listToReverse){
+                List<Integer> reversed = new ArrayList<>(); //reverse list variable to store the reversed list
+                for(int i = listToReverse.size()-1; i >= 0; i--){
+                    reversed.add(listToReverse.get(i));
+                }return reversed;
+            }
 
     public static List<Integer> Task4(List<Integer> a, List<Integer> b) {
         List<Integer> result = new ArrayList<>(); //created result variable of type List<integer> to store the result
-        //reverse list b and store in reversedList
-        public static List<Integer> reverseList(List<Integer> listToReverse){
-            List<Integer> reversed = new ArrayList<>(); //reverse list variable to store the reversed list
-            for(int i = listToReverse.size()-1; i >= 0; i--){
-                reversed.add(b.get(i));
-            }return reversedList;
-        }
         //check if the two lists are of the same size
         if (a.size() != b.size()){
                 return null;
