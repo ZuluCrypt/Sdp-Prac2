77ea965ba0159b39ebd423529079d6d5348bd9b1
Xanda555 <112818610+Xanda555@users.noreply.github.com>
2024-05-20 17:50:02 +0200

    Update SimpleFunctions.java

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,25 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
-    //reverse list b and store in reversedList
-    private static List<Integer> reverseList(List<Integer> listToReverse){
-                List<Integer> reversed = new ArrayList<>(); //reverse list variable to store the reversed list
-                for(int i = listToReverse.size()-1; i >= 0; i--){
-                    reversed.add(listToReverse.get(i));
-                }return reversed;
-            }
+   public static int sumByIndexes(List<Integer> a, List<Integer> b) {
+        int sum = 0;
 
-    public static List<Integer> Task4(List<Integer> a, List<Integer> b) {
-        List<Integer> result = new ArrayList<>(); //created result variable of type List<integer> to store the result
-        //check if the two lists are of the same size
-        if (a.size() != b.size()){
-                return null;
-        }else{  //if the two lists are of the same size then add each element in a to the corresponding element in the reversed list of b
-            List<Integer> reversedList = reverseList(b); //reverse list b 
-            for(int i = 0 ; i < a.size(); i++){
-                result.add(a.get(i) * reversedList.get(i));
+        for (int index : b) {
+            if (index >= 0 && index < a.size()) {
+                sum += a.get(index);
             }
         }
-        return result;     
+
+        return sum;
     }
 }
